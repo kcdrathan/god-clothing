f526269931bff9a6c0ed36d12a26c815d89f188e
Chandra Deep Rathan K <kcdrathan@gmail.com>
2021-02-08 15:31:34 +0530

    broken homepage in to smaller components and finished styling our menu-items

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import DirectoryMenu from "../components/directory-menu/directoryMenu";
+
+import "./homepage.scss";
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+}
